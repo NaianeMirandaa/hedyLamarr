f9017b06bd146251d5396508c87a15897598dc87
Naiane Miranda de Jesus <naianemjesus@gmail.com>
2023-10-12 23:17:57 -0300

    botões de avanço e recuuo da página "vida"

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,6 +1,20 @@
-const botaoPrincipal = document.querySelector('.botao_principal'),
-      botaoSecundario = document.querySelector('.botao_secundario');
+const buttons_back = document.querySelectorAll('.button_back'),
+      buttons_next = document.querySelectorAll('.button_next'),
+      blocos = document.querySelectorAll('.bloco');
 
+let indexAtual = 0;
 
-botaoPrincipal.addEventListener('click',()=>{botaoSecundario.classList.toggle('open')
-botaoPrincipal.classList.toggle('open');})
+buttons_back.forEach(button_back => {
+    button_back.addEventListener('click',()=>{
+        blocos[indexAtual].classList.remove('pagina_atual');
+        indexAtual--;
+        blocos[indexAtual].classList.add('pagina_atual');
+    });
+});
+buttons_next.forEach(button_next => {
+    button_next.addEventListener('click',()=>{
+        blocos[indexAtual].classList.remove('pagina_atual');
+        indexAtual++;
+        blocos[indexAtual].classList.add('pagina_atual');
+    });
+});
