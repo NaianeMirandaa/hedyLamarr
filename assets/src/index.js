c7d37ac5b3440b2b86dfdce939e03c05a1e219f2
Naiane Miranda de Jesus <naianemjesus@gmail.com>
2023-10-13 09:11:15 -0300

    filmografia completa

--- assets/src/index.js
+++ assets/src/index.js
@@ -18,3 +18,39 @@ buttons_next.forEach(button_next => {
         blocos[indexAtual].classList.add('pagina_atual');
     });
 });
+
+// galeria filmografia
+
+const controls = document.querySelectorAll('.controls');
+const filmeGaleria = document.querySelectorAll('.filme_galeria');
+const maxFilmes = filmeGaleria.length;
+let filmeAtual = 0;
+
+
+controls.forEach(control=>{
+    control.addEventListener('click',()=>{
+        const voltar = control.classList.contains('button_back--f');
+        
+        if(voltar){
+            filmeAtual--;
+        }else{
+            filmeAtual++
+        }
+        
+        if(filmeAtual >= maxFilmes){
+            filmeAtual = 0;
+        }
+        if(filmeAtual < 0){
+            filmeAtual = maxFilmes - 1;
+        }
+
+        filmeGaleria.forEach(filme=>{
+            filme.classList.remove('filme_atual');
+        })
+        filmeGaleria[filmeAtual].scrollIntoView({inline:'center',behavior:'smooth'});
+
+        filmeGaleria[filmeAtual].classList.add('filme_atual');
+
+        console.log('clicado',voltar, filmeAtual);
+    })
+});
